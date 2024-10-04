621da79f309166e2076ccff8369387ca46c2e754
Renato Alves <devrenatoalves@gmail.com>
2024-10-03 23:14:35 -0300

    Day 1 Fire Fast

---
+++ 6_EXPRESS/7_post_de_dados/index.js
@@ -0,0 +1,36 @@
+const express = require ("express");
+const app = express();
+const port = 3000; //Variavel ambiente
+const path = require ("path");
+
+const basePath = path.join(__dirname, 'templates');
+
+
+app.get('/users/add' , (req, res) => {
+
+  res.sendFile(`${basePath}/userform.html`)
+
+})
+
+app.post('/users/save' , (req, res) => {})
+
+app.get('/users/:id', (req, res) => {
+
+  const id = req.params.id
+
+  //leitura de tabela users, resgatar um usuario do banco
+  console.log(`Estamos buscando pelo usuario ${id}`)
+
+  res.sendFile(`${basePath}/users.html`)  
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${basePath}/index.html`)  
+});
+
+app.listen(port, () => {
+
+  console.log(`App rodando na porta ${port}`)
+
+});
+
