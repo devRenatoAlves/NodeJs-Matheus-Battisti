d35a12bbda4791680935f62379d78c7999d6c9ce
Renato Alves <devrenatoalves@gmail.com>
2024-10-12 15:47:34 -0300

    Reforming

--- 6_EXPRESS/8_modulo_rotas/index.js
+++ 6_EXPRESS/8_modulo_rotas/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000; //Variavel ambiente
 const path = require ("path");
 
-// Ler body
+const users = require ('./users')
 
+
+// Ler body
 app.use(
   express.urlencoded({
     extended: true,
@@ -15,32 +17,7 @@ app.use(express.json())
 
 const basePath = path.join(__dirname, 'templates');
 
-app.get('/users/add' , (req, res) => {
-
-  res.sendFile(`${basePath}/userform.html`)
-
-})
-
-app.post('/users/save' , (req, res) => {
-  console.log(req.body)
-
-  const name = req.body.name
-  const age = req.body.age
-
-  console.log(`The user name is ${name} and he have ${age} years old!`)
-
-  res.sendFile(`${basePath}/userform.html`)
-})
-
-app.get('/users/:id', (req, res) => {
-
-  const id = req.params.id
-
-  //leitura de tabela users, resgatar um usuario do banco
-  console.log(`Estamos buscando pelo usuario ${id}`)
-
-  res.sendFile(`${basePath}/users.html`)  
-});
+app.use('./users', users)
 
 app.get('/', (req, res) => {
   res.sendFile(`${basePath}/index.html`)  
---
+++ 6_EXPRESS/8_modulo_rotas/users/index.js
@@ -0,0 +1,32 @@
+const express = require ('express');
+const router = express.Router();
+const path = require ('path');
+
+const basePath = path.join(__dirname, 'templates');
+
+router.get('/add' , (req, res) => {
+
+  res.sendFile(`${basePath}/userform.html`)
+
+})
+
+app.post('/save' , (req, res) => {
+  console.log(req.body)
+
+  const name = req.body.name
+  const age = req.body.age
+
+  console.log(`The user name is ${name} and he have ${age} years old!`)
+
+  res.sendFile(`${basePath}/userform.html`)
+})
+
+app.get('/:id', (req, res) => {
+
+  const id = req.params.id
+
+  //leitura de tabela users, resgatar um usuario do banco
+  console.log(`Estamos buscando pelo usuario ${id}`)
+
+  res.sendFile(`${basePath}/users.html`)  
+});
